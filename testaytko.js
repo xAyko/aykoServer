7a9fbd1b4a9d42f8237bac71be528e2c02499806
Ayko <59120179+xAyko@users.noreply.github.com>
2021-02-28 16:58:34 +0100

    Update testaytko.js

--- testaytko.js
+++ testaytko.js
@@ -6,3 +6,8 @@ app.get('testaytko.js', function(request, response) {
   console.log('Works!');
   response.send('It works!');
 });
+
+
+app.listen(3000, function(){
+  console.log("App listening!");
+});
