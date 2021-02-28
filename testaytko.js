8a0b427d91de8599c5bdaeeac93a18273bb13ac4
Ayko <59120179+xAyko@users.noreply.github.com>
2021-02-28 16:49:31 +0100

    Update testaytko.js

--- testaytko.js
+++ testaytko.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 
-app.get('/testaytko.js', function(request, response) {
+app.get('testaytko.js', function(request, response) {
   console.log('Works!');
   response.send('It works!');
 });
